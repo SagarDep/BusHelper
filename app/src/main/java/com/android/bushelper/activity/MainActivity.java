9cc411bdd77d28cc050e77164d29772d9aee4da0
Cookie <tsubasa-kun@foxmail.com>
2016-04-29 17:28:32 +0800

    微调代码（强迫症啊）

--- app/src/main/java/com/android/bushelper/activity/MainActivity.java
+++ app/src/main/java/com/android/bushelper/activity/MainActivity.java
@@ -107,6 +107,5 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         MenuItem mnu1 = menu.add(0, 0, 0, "detail");
         mnu1.setIcon(R.mipmap.icon_detail);
         mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-
     }
 }
