3dc8ee90f3242918b565f7ce2d723d66dfbaedc0
Cookie <tsubasa-kun@foxmail.com>
2016-05-03 14:44:12 +0800

    线路和站点页面添加ListView

--- app/src/main/java/com/android/bushelper/fragment/LineFragment.java
+++ app/src/main/java/com/android/bushelper/fragment/LineFragment.java
@@ -5,14 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.android.bushelper.R;
 
 public class LineFragment extends Fragment {
 
+    private ListView lineList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_line, container, false);
+        lineList = (ListView)root.findViewById(R.id.line_list);
         return root;
     }
 
--- app/src/main/java/com/android/bushelper/fragment/StationFragment.java
+++ app/src/main/java/com/android/bushelper/fragment/StationFragment.java
@@ -5,14 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.android.bushelper.R;
 
 public class StationFragment extends Fragment {
 
+    private ListView stationList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_station, container, false);
+        stationList = (ListView)root.findViewById(R.id.station_list);
         return root;
     }
 
