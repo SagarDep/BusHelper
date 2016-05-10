8768aa99f0adba631fd3de5e00afcd992fca53e8
Cookie <tsubasa-kun@foxmail.com>
2016-05-10 15:50:11 +0800

    初步完成我的页面

--- app/src/main/java/com/android/bushelper/activity/MineActivity.java
+++ app/src/main/java/com/android/bushelper/activity/MineActivity.java
@@ -2,14 +2,47 @@ package com.android.bushelper.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.bushelper.R;
+import com.android.bushelper.app.MyApplication;
 
 public class MineActivity extends AppCompatActivity {
+
+    private TextView usernameTV;
+    private LinearLayout myOrderLL;
+    private LinearLayout resetPasswordLL;
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mine);
+        usernameTV = (TextView)findViewById(R.id.username_tv);
+        usernameTV.setText(MyApplication.user.getNickname());
+        myOrderLL = (LinearLayout)findViewById(R.id.my_order_ll);
+        myOrderLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        resetPasswordLL = (LinearLayout)findViewById(R.id.reset_password_ll);
+        resetPasswordLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        logoutBtn = (Button)findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
